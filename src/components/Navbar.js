171e63e032c2ebf381a2644658e537c4c2178af1
zshdevnet <zshdev.net@gmail.com>
2025-06-20 14:29:32 -0400

    all up to date

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
             <ul className="nav-links">
                 <li><Link to="/" className="nav-link">Home</Link></li>
                 <li><Link to="/about" className="nav-link">About</Link></li>
-                <li><Link to="/contactus" className="nav-link">Contact Us</Link></li>
+                <li><Link to="/contact" className="nav-link">Contact Us</Link></li>
             </ul>
         </nav>
     );
